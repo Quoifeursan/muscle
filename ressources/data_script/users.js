6f03acc427f04e4f77dc398206512e9d1ce5a73d
quoifeur <arthur.hya@gmail.com>
2026-06-18 13:55:00 +0200

    ajout user

--- ressources/data_script/users.js
+++ ressources/data_script/users.js
@@ -1,5 +1,6 @@
 const users = [
   { username: "Arthur", globalScore: 625, forceScore: 450, cardioScore: 800 },
+  { username: "Tom", globalScore: 213, forceScore: 425, cardioScore: 0 },
   { username: "Morgan", globalScore: 725, forceScore: 700, cardioScore: 750 },
   { username: "Tom", globalScore: 0, forceScore: 0, cardioScore: 0 },
   { username: "Adrien", globalScore: 0, forceScore: 0, cardioScore: 0 },
