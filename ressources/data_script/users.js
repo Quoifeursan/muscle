f615f4e0cd5524b0581de950d4adcd577af70869
quoifeur <arthur.hya@gmail.com>
2026-06-18 13:56:49 +0200

    correction bug de duplication d'uttilisateurs

--- ressources/data_script/users.js
+++ ressources/data_script/users.js
@@ -2,7 +2,6 @@ const users = [
   { username: "Arthur", globalScore: 625, forceScore: 450, cardioScore: 800 },
   { username: "Tom", globalScore: 213, forceScore: 425, cardioScore: 0 },
   { username: "Morgan", globalScore: 725, forceScore: 700, cardioScore: 750 },
-  { username: "Tom", globalScore: 0, forceScore: 0, cardioScore: 0 },
   { username: "Adrien", globalScore: 0, forceScore: 0, cardioScore: 0 },
   { username: "Simon", globalScore: 0, forceScore: 0, cardioScore: 0 },
   { username: "William", globalScore: 1113, forceScore: 475, cardioScore: 1750 },
