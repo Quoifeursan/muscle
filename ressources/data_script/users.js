4b1b56fee593779600bbfebd63e0a867d30a05b6
quoifeur <arthur.hya@gmail.com>
2026-06-22 18:08:35 +0200

    update user

--- ressources/data_script/users.js
+++ ressources/data_script/users.js
@@ -1,5 +1,5 @@
 const users = [
-{ username: "Arthur", globalScore: 1100, forceScore: 450, cardioScore: 1750 },
+{ username: "Arthur", globalScore: 1450, forceScore: 950, cardioScore: 1950 },
   { username: "Tom", globalScore: 213, forceScore: 425, cardioScore: 0 },
   { username: "Morgan", globalScore: 725, forceScore: 700, cardioScore: 750 },
   { username: "Adrien", globalScore: 0, forceScore: 0, cardioScore: 0 },
